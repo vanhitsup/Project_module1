d1748236f085ea054afa083a6b8bf23f39334f0d
ptva <vietanhbale@gmail.com>
2023-07-26 01:41:51 +0700

    1

---
+++ assets/js/login.js
@@ -0,0 +1,46 @@
+// Lấy dữ liệu local về
+let users = JSON.parse(localStorage.getItem("users")) || [];
+console.log(users);
+// Truy vấn các thẻ HTML form cần thiết
+let formLogin = document.getElementById("form");
+let userLogin = document.getElementById("username");
+let passLogin = document.getElementById("password");
+// console.log(formLogin, userLogin, passLogin);
+
+// Truy vấn các HTML báo lỗi
+let usernameErr = document.getElementById("userNameErr");
+let passErr = document.getElementById("passWordErr");
+
+// Hàm xóa lỗi
+function deleteErr() {
+  usernameErr.innerText = "";
+  passErr.innerText = "";
+}
+
+formLogin.onsubmit = function login(e) {
+  e.preventDefault();
+  //   console.log("kiểm tra");
+  let userLoginValue = userLogin.value;
+  let passLoginValue = passLogin.value;
+  // Khai báo biến user bằng việc sử dụng phương thức find() với điều kiện theo username
+  // Trả về toàn bộ thông tin (thuộc tính-giá trị) của đối tượng tìm thấy
+  let user = users.find((user) => user.userName === userLoginValue);
+
+  if (userLoginValue == "") {
+    usernameErr.innerText = "Username không được bỏ trống";
+  } else if (!user) {
+    deleteErr();
+    usernameErr.innerText = "Username không tồn tại";
+  } else {
+    deleteErr();
+    if (passLoginValue !== user.password) {
+      deleteErr();
+      passErr.innerText = "Mật khẩu không đúng";
+    } else {
+      deleteErr();
+      // Nếu đăng nhập thành công thì mọi người chuyển sang trang chủ
+        alert("Đăng nhập thành công");
+      window.location.href = "/index.html";
+    }
+  }
+};
---
+++ assets/js/register.js
@@ -0,0 +1,82 @@
+// Lấy các phần tử HTML cần thiết
+let registerForm = document.getElementById("form");
+let usernameInput = document.getElementById("username");
+let emailInput = document.getElementById("email");
+let passwordInput = document.getElementById("password");
+let passwordRepeatInput = document.getElementById("re-password");
+
+// Lấy các phần tử HTML để hiển thị thông báo lỗi
+let usernameError = document.getElementById("userNameErr");
+let emailError = document.getElementById("emailErr");
+let passwordError = document.getElementById("passErr");
+let passwordRepeatError = document.getElementById("rePassErr");
+console.log(usernameError, emailError, passwordError, passwordRepeatError);
+// Hàm xóa lỗi
+function deleteError() {
+  usernameError.innerText = "";
+  emailError.innerText = "";
+  passwordError.innerText = "";
+  passwordRepeatError.innerText = "";
+}
+// Hàm kiểm tra định dạng email (search trên mạng)
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+// Tạo ra 1 mảng để chứa đối tượng user khi khởi tạo thành công
+let users = JSON.parse(localStorage.getItem("users")) || [];
+// Hàm kiểm tra trùng username trong mảng users
+function checkUsername(username) {
+  // Tìm index của user có userName trùng với username được truyền vào
+  let findUserIndex = users.findIndex((user) => user.userName === username);
+  // Nếu tìm thấy user có userName trùng với username được truyền vào, trả về true, ngược lại trả về false
+  return findUserIndex !== -1;
+}
+
+// Sự kiện onsubmit - submit
+registerForm.onsubmit = function register(e) {
+  e.preventDefault(); // ngăn chặn cách xử lý mặc định của trình duyệt khi xảy ra sự kiện
+  // console.log("kiểm tra");
+  // Lấy giá trị trong input
+  let usernameValue = usernameInput.value;
+  let emailValue = emailInput.value;
+  let passwordValue = passwordInput.value;
+  let passwordRepeatValue = passwordRepeatInput.value;
+  // console.log(usernameValue, emailValue, passwordValue, passwordRepeatValue);
+  if (checkUsername(usernameValue)) {
+    usernameError.innerText = "Tên đăng nhập đã tồn tại";
+  } else if (usernameValue === "") {
+    usernameError.innerText = "Không được để trống tên người dùng";
+  } else {
+    deleteError();
+    if (isValidEmail(emailValue)) {
+      deleteError();
+      if (passwordValue.length >= 6) {
+        deleteError();
+        if (passwordRepeatValue == passwordValue) {
+          deleteError();
+          // Tạo ra newUser để lưu thông tin của người dùng mới đăng ký
+          let newUser = {
+            id: Math.floor(Math.random() * 10000000),
+            userName: usernameValue,
+            email: emailValue,
+            password: passwordValue,
+          };
+          users.push(newUser);
+          // Lưu dữ liệu lên local
+          localStorage.setItem("users", JSON.stringify(users));
+          // Chuyển hướng đến trang đăng nhập sau khi đăng ký thành công
+         
+          alert("Đăng ký thành công");
+          window.location.href="login.html";
+        } else {
+          passwordRepeatError.innerText = "Mật khẩu không trùng khớp";
+        }
+      } else {
+        passwordError.innerText = "Mật khẩu không đúng định dạng";
+      }
+    } else {
+      emailError.innerText = "Email không đúng";
+    }
+  }
+};
